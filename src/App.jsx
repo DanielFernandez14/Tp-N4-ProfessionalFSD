df4b4dca2b1f07ef404c8f5e848f1eafcbff55a2
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-09-29 19:22:43 -0300

    Tarjetas.jsx y app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Tarjeta }  from "./Components/Tarjeta/Tarjeta.jsx"
 
 function App() {
   return (
-    <>
-    </>
+      <div className="app">
+        <Tarjeta
+        nombre="Daniel Fernandez"
+        profesion="Desarrollador Web"
+        imagen="/Personajes/Personaje1.png"
+        descripcion="Apasionado por la tecnología y el desarrollo web, con experiencia en múltiples proyectos y un enfoque en la creación de soluciones innovadoras."
+        />
+        <Tarjeta
+        nombre="Maria Gomez"
+        profesion="Diseñadora Gráfica"
+        imagen="/Personajes/Personaje2.png"
+        descripcion="Creativa y detallista, especializada en diseño gráfico y branding. Comprometida con la creación de identidades visuales impactantes."
+        />
+        <Tarjeta
+        nombre="Luis Martinez"
+        profesion="Especialista en Marketing Digital"
+        imagen="/Personajes/Personaje3.png"
+        descripcion="Experto en estrategias de marketing digital, SEO y gestión de redes sociales. Enfocado en aumentar la visibilidad y el alcance de las marcas."
+        />
+        <Tarjeta
+        nombre="Ana Rodriguez"
+        profesion="Gestora de Proyectos"
+        imagen="/Personajes/Personaje4.png"
+        descripcion="Profesional en gestión de proyectos con habilidades en liderazgo y organización. Experiencia en la coordinación de equipos multidisciplinarios."
+        />
+      </div>
   )
 }
 
---
+++ src/Components/Tarjeta/Tarjeta.jsx
@@ -0,0 +1,14 @@
+import "./Tarjeta.css"
+
+function Tarjeta({ nombre, profesion, imagen, descripcion }) {
+    return (
+            <div className="tarjeta">
+                <img src={imagen} alt={`Foto de ${nombre}`} className="tarjeta-imagen"/>
+                <h2>{nombre}</h2>
+                <h3>{profesion}</h3>
+                <p>{descripcion}</p>
+            </div>
+    )
+}
+
+export { Tarjeta }
